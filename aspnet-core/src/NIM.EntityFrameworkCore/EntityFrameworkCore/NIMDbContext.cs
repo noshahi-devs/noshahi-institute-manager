17e556fd1e8b2c2eaf21476479f6e4528b4ab825
noshahi-devs <noshahidevelopersinc@gmail.com>
2025-06-08 08:21:15 +0500

    Created Campus Entity in DB

--- aspnet-core/src/NIM.EntityFrameworkCore/EntityFrameworkCore/NIMDbContext.cs
+++ aspnet-core/src/NIM.EntityFrameworkCore/EntityFrameworkCore/NIMDbContext.cs
@@ -3,15 +3,19 @@ using NIM.Authorization.Roles;
 using NIM.Authorization.Users;
 using NIM.MultiTenancy;
 using Microsoft.EntityFrameworkCore;
+using NIM.Entities; // Make sure this using is at the top
 
 namespace NIM.EntityFrameworkCore;
 
 public class NIMDbContext : AbpZeroDbContext<Tenant, Role, User, NIMDbContext>
 {
-    /* Define a DbSet for each entity of the application */
+    // Entity Framework Core DbSets
+    //Campuses
+    public DbSet<Campus> Campuses { get; set; }
 
     public NIMDbContext(DbContextOptions<NIMDbContext> options)
         : base(options)
     {
+
     }
 }
